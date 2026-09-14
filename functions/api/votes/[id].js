3a80f3a1c268dad2ae440578af51618fcd2ccff9
DodoNonProgramma <40170160+DodoNonProgrammaNeOraNeMais@users.noreply.github.com>
2026-09-14 13:39:27 +0200

    Delete functions/api/votes/[id].js

--- functions/api/votes/[id].js
+++
@@ -1,5 +0,0 @@
-// DELETE /api/admin/votes/:id -> forza l'eliminazione di un singolo voto
-export async function onRequestDelete({ params, env }) {
-  await env.DB.prepare(`DELETE FROM votes WHERE id = ?`).bind(params.id).run();
-  return Response.json({ ok: true });
-}
